c8d3028f6388ed84a76010815e18fac6b1cbe17b
PetrSKS <157022931+PetrSKS@users.noreply.github.com>
2025-01-09 15:54:24 +0100

    Update 30s.py

--- 30s.py
+++ 30s.py
@@ -311,6 +311,9 @@ def main():
 
     # Extrakce metrik
     extracted_metrics = extract_metrics_from_json(json_data, required_metrics)
+    st.write(extracted_metrics)
+    df = extracted_metrics.copy()
+
     df = pd.DataFrame.from_dict(extracted_metrics, orient='index')
     df.columns = ['Domácí', 'Hosté']
     df = df.reindex(required_metrics)  # Seřadit podle `required_metrics`
