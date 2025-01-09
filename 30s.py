145a4498603a5545e86b645a5dedf913d8a51009
PetrSKS <157022931+PetrSKS@users.noreply.github.com>
2025-01-09 12:24:56 +0100

    Update 30s.py

--- 30s.py
+++ 30s.py
@@ -10,6 +10,7 @@ from PIL import Image
 from io import BytesIO
 from collections import Counter
 import numpy as np
+from streamlit_autorefresh import st_autorefresh
 
 # -- Selenium
 from selenium import webdriver
