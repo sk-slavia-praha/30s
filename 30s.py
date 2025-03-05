374bedac3427e4bb161944cbed4c3d256ba0f6cb
PetrSKS <157022931+PetrSKS@users.noreply.github.com>
2025-03-05 11:14:41 +0100

    Update 30s.py

--- 30s.py
+++ 30s.py
@@ -258,7 +258,16 @@ def main():
         posledni_zapas_id = 0
 
     # Možnost vybrat zápas podle názvu
-    vybrany_zapas = st.selectbox("Vyber zápas", match_list["Home_team - Away_team"].tolist())
+    possible_column_names = ["Home_team - Away_team", "Home_team_Away_team"]
+
+    # Najdeme existující sloupec v datech
+    existing_column = next((col for col in match_list.columns if col in possible_column_names), None)
+
+    if existing_column:
+        vybrany_zapas = st.selectbox("Vyber zápas", match_list[existing_column].tolist())
+    else:
+        st.error("Sloupec s názvem týmu nebyl nalezen.")
+    #vybrany_zapas = st.selectbox("Vyber zápas", match_list["Home_team - Away_team"].tolist())
     match_id = match_list[match_list["Home_team - Away_team"] == vybrany_zapas]["match_id"].values[0]
     #match_id = st.number_input("Zadej match_id", value=12580787)
     #datum = "10.01.2025"
