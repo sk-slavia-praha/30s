5a3da9bc3b96f7c54295d5fd96d8ff05dd25dfda
PetrSKS <157022931+PetrSKS@users.noreply.github.com>
2025-03-08 11:31:23 +0100

    Update 30s.py

--- 30s.py
+++ 30s.py
@@ -279,7 +279,7 @@ def main():
         raise KeyError("Sloupec s názvem týmu nebyl nalezen.")
     #vybrany_zapas = st.selectbox("Vyber zápas", match_list["Home_team - Away_team"].tolist())
     #match_id = match_list[match_list["Home_team - Away_team"] == vybrany_zapas]["match_id"].values[0]
-    #match_id = st.number_input("Zadej match_id", value=12580787)
+    match_id = st.number_input("Zadej match_id", value=12580796)
     #datum = "10.01.2025"
 
 
