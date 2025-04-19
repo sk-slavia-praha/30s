05aee0e749f765904eef74d259bfcc7bb1490e0d
PetrSKS <157022931+PetrSKS@users.noreply.github.com>
2025-04-19 13:51:11 +0200

    Update 30s.py

--- 30s.py
+++ 30s.py
@@ -268,7 +268,7 @@ def main():
     #     match_id = match_list[match_list[existing_column] == vybrany_zapas]["match_id"].values[0]
     #else:
     #    raise KeyError("Sloupec s názvem týmu nebyl nalezen.")
-    match_id = st.selectbox("Vyber zápas", options=[12437640])
+    match_id = st.selectbox("Vyber zápas", options=[12448212])
 
     # -----------------------------------------------------------------------------
     # 1) Stažení JSONu pro momentum
